257a84cc05624040b46d3af5f009f0108876a3cc
MrKauperwood <42598865+MrKauperwood@users.noreply.github.com>
2019-09-26 19:24:52 +0300

    Edit name function

--- test_items.py
+++ test_items.py
@@ -1,5 +1,5 @@
 link = r"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def test_button_finder(browser):
+def test_find_basket_button(browser):
     browser.get(link)
     assert browser.find_element_by_css_selector(".btn-add-to-basket")
